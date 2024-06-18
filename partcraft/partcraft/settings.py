d3297e891ca867435f48a40e5627ee95337a2339
Nandhaad3 <nandhakumar@ad3infotech.com>
2024-06-18 12:20:14 +0530

    update

--- partcraft/partcraft/settings.py
+++ partcraft/partcraft/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 
-import dj_database_url
+# import dj_database_url
 from django.conf import settings
 import os
 import dj_database_url
