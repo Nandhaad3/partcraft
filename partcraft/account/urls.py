c58a4a259160c59f6049a46d408c40ffbb18bf2a
Nandhakumar <nandhakumar@ad3infotech.com>
2024-08-29 11:10:56 +0530

    Update urls.py

--- partcraft/account/urls.py
+++ partcraft/account/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from  .views import *
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
      path('register/', UserRegistrationView.as_view(), name='register'),
      path('verify/', VerifyEmailView.as_view(), name='verify'),
      path('login/', UserLoginView.as_view(), name='login'),
