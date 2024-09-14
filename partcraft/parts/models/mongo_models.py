c58b06ec4363fc542319a08e036e311129bd32da
Dhanushpathi Prakash <78605418+DhanushpathiPrakash@users.noreply.github.com>
2024-09-14 12:38:31 +0530

    update

--- partcraft/parts/models/mongo_models.py
+++ partcraft/parts/models/mongo_models.py
@@ -16,3 +16,17 @@ class product_attribute(Document):
     def __str__(self):
         return self.product_code
 
+class product_document(Document):
+    TYPE_CHOICE = ('Internal', 'External')
+    product_code = fields.StringField(required=True)
+    document_id = fields.StringField(required=True)
+    document_name = fields.StringField(required=True)
+    document_url = fields.StringField(required=True)
+    document_type = fields.StringField(choices=TYPE_CHOICE, required=True)
+
+    meta = {
+        'collection': 'product_documents',
+        'db_alias': 'nonrel'
+    }
+    def __str__(self):
+        return self.product_code
---
+++ partcraft/parts/mongo_admin.py
@@ -0,0 +1,6 @@
+from django_mongoengine import mongo_admin
+from models.mongo_models import product_attribute
+
+class ProductAttributeAdmin(mongo_admin.DocumentAdmin):
+    list_display = ('product_code')
+mongo_admin.site.register(product_attribute, ProductAttributeAdmin)
